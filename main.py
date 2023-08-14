94cd259216a0e73276d6706d6edd127e29a23b15
John Ivan Puayap <johnivanpuayap@gmail.com>
2023-08-14 16:11:03 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# Set Up Beautiful Soup
+# Set Up Selenium
+# Go to the website and search for property
+# Scrape the price, address and link of every listing
+# Create a list of classes to save the listings
+# Save the list by entering it on Google Forms
